aa0f3b91052453819e6bbc2c60f3ed84c6d2d389
kyogo <kyogoammy@gmail.com>
2025-06-05 18:55:07 +0900

    コード全体の見直し

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py
 
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from blueprints.alb import alb_bp
 from blueprints.classroom import classroom_bp
 from blueprints.callback import callback_bp
@@ -8,6 +9,9 @@ from blueprints.link import link_bp
 from blueprints.admin import admin_bp
 
 app = Flask(__name__)
+app.secret_key = "your-secret-key"  # CSRF用に必須
+
+csrf = CSRFProtect(app)
 
 # 各機能モジュールをBlueprintとして登録
 app.register_blueprint(alb_bp)
