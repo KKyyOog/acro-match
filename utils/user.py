5cac2d521a167e8513652687f3b0efa05cb90907
kyogo <kyogoammy@gmail.com>
2025-06-08 14:47:55 +0900

    webhookIDの取得と返答

--- utils/user.py
+++ utils/user.py
@@ -1,7 +1,7 @@
 # utils/user.py
 from utils.sheets import (
     append_row_if_new_user,
-    update_app_liff_id_by_name_birthday
+    update_liff_id_by_name_and_birthday4
 )
 from utils.logging_util import log_exception
 from datetime import datetime
@@ -10,12 +10,14 @@ def register_user_info(name: str, birthday: str, chat_liff_id: str = "", app_lif
     try:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        # 基本登録
-        append_row_if_new_user(name, birthday, chat_liff_id=chat_liff_id, app_liff_id=app_liff_id, timestamp=timestamp)
+        # アプリ側 LIFF ID のみ更新する場合（名前 + 誕生日下4桁）
+        if name and birthday and app_liff_id:
+            digits = ''.join(filter(str.isdigit, birthday))
+            if len(digits) >= 4 and update_liff_id_by_name_and_birthday4(name, digits[-4:], app_liff_id):
+                return
 
-        # アプリ側の LIFF ID がある場合は更新
-        if app_liff_id and name and birthday:
-            update_app_liff_id_by_name_birthday(name, birthday, app_liff_id)
+        # それ以外は新規追加 or 情報補完
+        append_row_if_new_user(name, birthday, chat_liff_id=chat_liff_id, app_liff_id=app_liff_id, timestamp=timestamp)
 
     except Exception as e:
         log_exception(e, context="register_user_info 処理")
