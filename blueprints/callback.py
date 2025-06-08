b2b926e381e5ab7693290d1b2dbd5d4ec5a39249
kyogo <kyogoammy@gmail.com>
2025-06-08 14:10:31 +0900

    webhookIDの取得と返答

--- blueprints/callback.py
+++ blueprints/callback.py
@@ -43,7 +43,7 @@ def receive_callback():
                         name, bday = parts[0].strip(), parts[1].strip()
                         if re.match(r"^\d{4}年\d{1,2}月\d{1,2}日$", bday):
                             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                            register_user_info(name, bday, user_id, webhook_event_id=webhook_id)
+                            register_user_info(name, bday, user_id)
                             send_line_message(user_id, f"{name} さん、登録ありがとうございます！\n生年月日 {bday} も登録しました。")
                             user_states[user_id] = {'name': name}
                             continue
